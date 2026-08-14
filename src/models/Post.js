d6343c606dda8197ff848fc38bc8c191a6a19929
Adeoluwa12 <adeoyeoluwaferanmi@gmail.com>
2026-08-14 23:21:19 +0100

    added file storage o

--- src/models/Post.js
+++ src/models/Post.js
@@ -16,4 +16,4 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Post", postSchema);
+export default mongoose.model("CloudsecPost", postSchema);
